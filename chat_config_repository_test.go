57728c2df955ffa13e24f13e6a092846193f8a71
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2015-11-29 00:26:05 +0300

    Tests added

--- chat_config_repository_test.go
+++ chat_config_repository_test.go
@@ -12,19 +12,25 @@ func TestChatConfigRepository(t *testing.T) {
 		Convey("When adding new config", func() {
 			m.ChatConfigRepository.Set(100500, "{\"a\": 100}")
 
-			Convey("It can be found in repository", func(){
+			Convey("It can be found in repository", func() {
 				So(m.ChatConfigRepository.Get(100500), ShouldEqual, "{\"a\": 100}")
 			})
 		})
 
-		Convey("With existing config", func(){
+		Convey("With existing config", func() {
 			m.ChatConfigRepository.Set(100500, "{\"a\": 100}")
 
-			Convey("When removing config", func(){
+			Convey("When removing config", func() {
 				m.ChatConfigRepository.Remove(100500)
-				Convey("It cannot be found in repository", func(){
+				Convey("It cannot be found in repository", func() {
 					So(m.ChatConfigRepository.Get(100500), ShouldBeEmpty)
 				})
+			})
+		})
+
+		Convey("When trying to get missing chatID", func() {
+			Convey("Response should be empty", func(){
+				So(m.ChatConfigRepository.Get(100500), ShouldBeEmpty)
 			})
 		})
 	})
