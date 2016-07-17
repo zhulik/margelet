e47dc5f464cecf87091c2ffab11e0e52f972da30
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2016-07-17 13:48:43 +0300

    MargeletSession is public now

--- session.go
+++ session.go
@@ -4,7 +4,8 @@ import (
 	"gopkg.in/telegram-bot-api.v4"
 )
 
-type margeletSession struct {
+// MargeletSession incapsulates user's session
+type MargeletSession struct {
 	bot           MargeletAPI
 	chatID        int64
 	userID        int
@@ -13,7 +14,7 @@ type margeletSession struct {
 }
 
 func newMargetletSession(bot MargeletAPI, message *tgbotapi.Message, responses []tgbotapi.Message) Session {
-	return &margeletSession{
+	return &MargeletSession{
 		bot:           bot,
 		chatID:        message.Chat.ID,
 		userID:        message.From.ID,
@@ -22,54 +23,67 @@ func newMargetletSession(bot MargeletAPI, message *tgbotapi.Message, responses [
 	}
 }
 
-func (s *margeletSession) Responses() []tgbotapi.Message {
+// Responses returns all user's responses in session
+func (s *MargeletSession) Responses() []tgbotapi.Message {
 	return s.responses
 }
 
-func (s *margeletSession) QuickSend(text string) (tgbotapi.Message, error) {
+// QuickSend send test to session chat
+func (s *MargeletSession) QuickSend(text string) (tgbotapi.Message, error) {
 	return s.bot.QuickSend(s.chatID, text)
 }
 
-func (s *margeletSession) QuckReply(text string) (tgbotapi.Message, error) {
+// QuckReply send a reply to last session message
+func (s *MargeletSession) QuckReply(text string) (tgbotapi.Message, error) {
 	return s.bot.QuickReply(s.chatID, s.lastMessageID, text)
 }
 
-func (s *margeletSession) SendImageByURL(url string, caption string, replyMarkup interface{}) (tgbotapi.Message, error) {
+// SendImageByURL send image by url to session chat
+func (s *MargeletSession) SendImageByURL(url string, caption string, replyMarkup interface{}) (tgbotapi.Message, error) {
 	return s.bot.SendImageByURL(s.chatID, url, caption, replyMarkup)
 }
 
-func (s *margeletSession) SendTypingAction() error {
+// SendTypingAction send typing action to session chat
+func (s *MargeletSession) SendTypingAction() error {
 	return s.bot.SendTypingAction(s.chatID)
 }
 
-func (s *margeletSession) SendUploadPhotoAction() error {
+// SendTypingAction send upload photo action to session chat
+func (s *MargeletSession) SendUploadPhotoAction() error {
 	return s.bot.SendUploadPhotoAction(s.chatID)
 }
 
-func (s *margeletSession) SendRecordVideoAction() error {
+// SendRecordVideoAction send record video action to session chat
+func (s *MargeletSession) SendRecordVideoAction() error {
 	return s.bot.SendRecordVideoAction(s.chatID)
 }
 
-func (s *margeletSession) SendUploadVideoAction() error {
+// SendUploadVideoAction send upload video action to session chat
+func (s *MargeletSession) SendUploadVideoAction() error {
 	return s.bot.SendUploadVideoAction(s.chatID)
 }
 
-func (s *margeletSession) SendRecordAudioAction() error {
+// SendRecordAudioAction send record audio action to session chat
+func (s *MargeletSession) SendRecordAudioAction() error {
 	return s.bot.SendRecordAudioAction(s.chatID)
 }
 
-func (s *margeletSession) SendUploadAudioAction() error {
+// SendUploadAudioAction send upload audio action to session chat
+func (s *MargeletSession) SendUploadAudioAction() error {
 	return s.bot.SendUploadAudioAction(s.chatID)
 }
 
-func (s *margeletSession) SendUploadDocumentAction() error {
+// SendUploadDocumentAction send upload document action to session chat
+func (s *MargeletSession) SendUploadDocumentAction() error {
 	return s.bot.SendUploadDocumentAction(s.chatID)
 }
 
-func (s *margeletSession) SendFindLocationAction() error {
+// SendFindLocationAction send find location action to session chat
+func (s *MargeletSession) SendFindLocationAction() error {
 	return s.bot.SendFindLocationAction(s.chatID)
 }
 
-func (s *margeletSession) SendHideKeyboard(chatID int64, message string) error {
+// SendHideKeyboard send message with hidding keyboard to session chat
+func (s *MargeletSession) SendHideKeyboard(chatID int64, message string) error {
 	return s.bot.SendHideKeyboard(s.chatID, message)
 }
