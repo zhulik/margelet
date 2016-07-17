380b1a666ff2166bdec86e171b76ccd740a98218
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2016-07-17 13:33:41 +0300

    Some helpers added to session

--- session.go
+++ session.go
@@ -33,3 +33,43 @@ func (s *margeletSession) QuickSend(text string) (tgbotapi.Message, error) {
 func (s *margeletSession) QuckReply(text string) (tgbotapi.Message, error) {
 	return s.bot.QuickReply(s.chatID, s.lastMessageID, text)
 }
+
+func (s *margeletSession) SendImageByURL(url string, caption string, replyMarkup interface{}) (tgbotapi.Message, error) {
+	return s.bot.SendImageByURL(s.chatID, url, caption, replyMarkup)
+}
+
+func (s *margeletSession) SendTypingAction() error {
+	return s.bot.SendTypingAction(s.chatID)
+}
+
+func (s *margeletSession) SendUploadPhotoAction() error {
+	return s.bot.SendUploadPhotoAction(s.chatID)
+}
+
+func (s *margeletSession) SendRecordVideoAction() error {
+	return s.bot.SendRecordVideoAction(s.chatID)
+}
+
+func (s *margeletSession) SendUploadVideoAction() error {
+	return s.bot.SendUploadVideoAction(s.chatID)
+}
+
+func (s *margeletSession) SendRecordAudioAction() error {
+	return s.bot.SendRecordAudioAction(s.chatID)
+}
+
+func (s *margeletSession) SendUploadAudioAction() error {
+	return s.bot.SendUploadAudioAction(s.chatID)
+}
+
+func (s *margeletSession) SendUploadDocumentAction() error {
+	return s.bot.SendUploadDocumentAction(s.chatID)
+}
+
+func (s *margeletSession) SendFindLocationAction() error {
+	return s.bot.SendFindLocationAction(s.chatID)
+}
+
+func (s *margeletSession) SendHideKeyboard(chatID int64, message string) error {
+	return s.bot.SendHideKeyboard(s.chatID, message)
+}
